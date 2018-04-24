33daa4967501dfc1f77189459d30add6606ffaf5
Miguel San Juan <miguel.sanjuan@api-ux.com>
2018-04-24 18:24:16 -0300

    Modelo

---
+++ src/main/java/com/apiux/example/ldap/model/Persona.java
@@ -0,0 +1,35 @@
+package com.apiux.example.ldap.model;
+
+public class Persona {
+
+	private String idUsuario;
+	private String nombre;
+	private String email;
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
